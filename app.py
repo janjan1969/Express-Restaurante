e743bd2a6abd83256c6bffb89ba1ad43cb3ce737
Professor.Janderson <163478166+janjan1969@users.noreply.github.com>
2024-10-24 22:22:39 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+print("""𝔈𝔵𝔭𝔯𝔢𝔰𝔰 ℜ𝔢𝔰𝔱𝔞𝔲𝔯𝔞𝔫𝔱𝔢 """)
+
+print("1.Cadastrar restaurante")
+print("2.Listar restaurante")
+print("3.Ativar restaurante")
+print("4.Sair\n")
+
+opcao_escolhida = input(" Escolha uma opção:")
+print(f" Você escolheu a opção {opcao_escolhida}")
+
+
