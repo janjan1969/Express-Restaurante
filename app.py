c010d3d9243648101af5b30045529f864dc487a7
Professor.Janderson <163478166+janjan1969@users.noreply.github.com>
2024-10-24 22:45:33 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 print("""𝔈𝔵𝔭𝔯𝔢𝔰𝔰 ℜ𝔢𝔰𝔱𝔞𝔲𝔯𝔞𝔫𝔱𝔢 """)
 
-print("1.Cadastrar restaurante")
-print("2.Listar restaurante")
-print("3.Ativar restaurante")
-print("4.Sair\n")
+print("1. Cadastrar restaurante")
+print("2. Listar restaurante")
+print("3. Ativar restaurante")
+print("4. Sair\n")
 
-opcao_escolhida = input(" Escolha uma opção:")
-print(f" Você escolheu a opção {opcao_escolhida}")
+opcao_escolhida = int(input(" Escolha uma opção:"))
+#opcao_escolhida = int(opcao_escolhida)
+
+if opcao_escolhida == 1:
+    print(" Cadastrar restaurante ")
+elif opcao_escolhida == 2:
+    print(" Lista restaurante ")
+elif opcao_escolhida == 3:
+    print(" Ativa restaurante ")
+else:
+    print(" Encerrando o programa ")
+
 
 
